68478a54469f9305918d1932ecd663e3a10aef4a
muddybootscode <firsttogo98@gmail.com>
2020-07-28 15:38:37 -0400

    Trying out incremental rebuild

--- pages/[pageId].js
+++ pages/[pageId].js
@@ -2,22 +2,41 @@ import { NotionRenderer } from "react-notion";
 import Head from "next/head";
 import fetch from "node-fetch";
 
-// export async function getStaticPaths() {
-//   const data = await fetch(
-//     'https://notion-api.splitbee.io/v1/table/021eaf5abd07437a96cea8a909439121'
-//   ).then(res => res.json());
-//
-//   const paths = data.map((row) => {
-//     const id = row.slug[0]
-//     return `/${id}`
-//   })
-//
-//   return { paths, fallback: true}
-//
-// }
-//
-// export async function getStaticProps({ params }) {
-//   const pageId = params.pageId;
+export async function getStaticPaths() {
+  const data = await fetch(
+    'https://notion-api.splitbee.io/v1/table/021eaf5abd07437a96cea8a909439121'
+  ).then(res => res.json());
+
+  const paths = data.map((row) => {
+    const id = row.slug[0]
+    return `/${id}`
+  })
+
+  return { paths, fallback: true}
+
+}
+
+export async function getStaticProps({ params }) {
+  const pageId = params.pageId;
+
+  if (!pageId) {
+    return;
+  }
+
+  const data = await fetch(
+    `https://notion-api.splitbee.io/v1/page/${pageId}`
+  ).then(res => res.json());
+
+  return {
+    props: {
+      blockMap: data,
+      revalidate: 1
+    }
+  };
+}
+
+// export async function getServerSideProps(context) {
+//   const pageId = context.params?.pageId;
 //
 //   if (!pageId) {
 //     return;
@@ -33,24 +52,6 @@ import fetch from "node-fetch";
 //     }
 //   };
 // }
-
-export async function getServerSideProps(context) {
-  const pageId = context.params?.pageId;
-
-  if (!pageId) {
-    return;
-  }
-
-  const data = await fetch(
-    `https://notion-api.splitbee.io/v1/page/${pageId}`
-  ).then(res => res.json());
-
-  return {
-    props: {
-      blockMap: data
-    }
-  };
-}
 
 const NotionPage = ({ blockMap }) => {
   if (!blockMap || Object.keys(blockMap).length === 0) {
