0f875792c74935d88ca5ea6e0098ad5688dd6556
brsm <bradleysmith.dev@gmail.com>
2020-09-11 16:45:22 -0500

    Update [pageId].js

--- pages/[pageId].js
+++ pages/[pageId].js
@@ -2,39 +2,6 @@ import { NotionRenderer } from "react-notion";
 import Head from "next/head";
 import fetch from "node-fetch";
 
-// export async function getStaticPaths() {
-//   const data = await fetch(
-//     'https://notion-api.splitbee.io/v1/table/021eaf5abd07437a96cea8a909439121'
-//   ).then(res => res.json());
-//
-//   const paths = data.map((row) => {
-//     const id = row.slug[0]
-//     return `/${id}`
-//   })
-//
-//   return { paths, fallback: true}
-//
-// }
-//
-// export async function getStaticProps({ params }) {
-//   const pageId = params.pageId;
-//
-//   if (!pageId) {
-//     return;
-//   }
-//
-//   const data = await fetch(
-//     `https://notion-api.splitbee.io/v1/page/${pageId}`
-//   ).then(res => res.json());
-//
-//   return {
-//     props: {
-//       blockMap: data,
-//       revalidate: .5
-//     }
-//   };
-// }
-
 export async function getServerSideProps(context) {
   const pageId = context.params?.pageId;
 
